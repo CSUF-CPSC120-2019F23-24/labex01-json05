382021d008efb665d98a293617753fd9c4b88a27
json05 <json05@cs104u-1g.acad.fullerton.edu>
2019-08-24 14:34:31 -0700

    changed to addition

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -7,18 +7,18 @@ int main()
   double hours, rate, pay;
 
   // Get the number of hours worked.
-  std::cout << "How many pencils did you sell? ";
+  std::cout << "How many cookies do you have? ";
   std::cin >> hours;
 
   // Get the hourly pay rate.
-  std::cout << "How much does each pencil cost? ";
+  std::cout << "How many cookies does your friend have? ";
   std::cin >> rate;
 
   // Calculate the pay.
-  pay = hours * rate;
+  pay = hours + rate;
 
   // Display the pay.
-  std::cout << "You have earned $" << pay << ". Good Job!" << std::endl;
+  std::cout << "You have " << pay << " cookies in total!" << std::endl;
 
   return 0;
 }
