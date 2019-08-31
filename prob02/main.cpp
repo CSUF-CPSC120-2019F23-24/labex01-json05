8b9eb32d863103feb947dcbc8666c253108ecf87
json05 <json05@cs104u-1g.acad.fullerton.edu>
2019-08-31 13:18:42 -0700

    changed calculator

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -7,18 +7,18 @@ int main()
   double hours, rate, pay;
 
   // Get the number of hours worked.
-  std::cout << "How many cookies do you have? ";
+  std::cout << "How many candy bars did you sell? ";
   std::cin >> hours;
 
   // Get the hourly pay rate.
-  std::cout << "How many cookies does your friend have? ";
+  std::cout << "How much does the organization earn for each bar sold? ";
   std::cin >> rate;
 
   // Calculate the pay.
   pay = hours + rate;
 
   // Display the pay.
-  std::cout << "You have " << pay << " cookies in total!" << std::endl;
+  std::cout << "Congratulations! You have earned " << pay << " for the organization!" << std::endl;
 
   return 0;
 }
